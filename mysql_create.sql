2c1dabb50ec6fe43ca6f910d81f1097e5c6dea26
Hendricks, Brent Andrew <brent.hendricks@p5software.com>
2022-01-22 20:33:38 -0500

    Added simplified registrations and operators

--- mysql_create.sql
+++ mysql_create.sql
@@ -38,6 +38,34 @@ CREATE INDEX `registration` ON `registrations`.`registrations` (`registration` A
 
 
 -- -----------------------------------------------------
+-- Table `registrations`.`simple`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `registrations`.`simple` (
+  `unique_id` INT NOT NULL AUTO_INCREMENT,
+  `icao_hex` CHAR(6) NOT NULL,
+  `registration` VARCHAR(20) NOT NULL,
+  `data` JSON NOT NULL,
+  `hash` CHAR(32) NOT NULL,
+  `source` INT NOT NULL,
+  `created` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`unique_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+CREATE UNIQUE INDEX `unique_id_UNIQUE` ON `registrations`.`simple` (`unique_id` ASC) VISIBLE;
+
+CREATE UNIQUE INDEX `icao_hex_hash` ON `registrations`.`simple` (`icao_hex` ASC, `hash` ASC) VISIBLE;
+
+CREATE INDEX `icao_hex` ON `registrations`.`simple` (`icao_hex` ASC) VISIBLE;
+
+CREATE INDEX `hash` ON `registrations`.`simple` (`hash` ASC) VISIBLE;
+
+CREATE INDEX `registration` ON `registrations`.`simple` (`registration` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
 -- Table `registrations`.`sources`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `registrations`.`sources` (
@@ -47,6 +75,26 @@ CREATE TABLE IF NOT EXISTS `registrations`.`sources` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `registrations`.`operators`
+-- -----------------------------------------------------
+CREATE TABLE `operators` (
+  `unique_id` int NOT NULL AUTO_INCREMENT,
+  `airline_designator` varchar(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `callsign` varchar(45) NOT NULL,
+  `country` varchar(45) NOT NULL,
+  `hash` char(32) NOT NULL,
+  `created` datetime DEFAULT CURRENT_TIMESTAMP,
+  `deleted` datetime DEFAULT NULL,
+  PRIMARY KEY (`unique_id`),
+  UNIQUE KEY `unique_id_UNIQUE` (`unique_id`),
+  UNIQUE KEY `airline_designator_hash` (`airline_designator`,`hash`),
+  KEY `airline_designator` (`airline_designator`),
+  KEY `hash` (`hash`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
