d2ee21096d65adab1dd160ccff506b1e7ee54cb7
Brent Andrew Hendricks <brent.hendricks@p5software.com>
2022-03-07 21:11:51 -0500

    Initial commit

---
+++ api.py
@@ -0,0 +1,407 @@
+#!/usr/bin/env python3
+
+import http.server
+import socketserver
+from urllib import parse
+import json
+import sys
+import os
+import logging
+import logging.handlers as handlers
+import signal
+import mysql.connector #pip3 install mysql-connector-python
+
+
+def handle_interrupt(signal, frame):
+    raise sigKill("SIGKILL Requested")
+
+
+class sigKill(Exception):
+    pass
+
+
+class RequestHandler(http.server.SimpleHTTPRequestHandler):
+
+    #Create a conversation tracker for this request
+    def __init__(self, request, client_address, server):
+
+        http.server.SimpleHTTPRequestHandler.__init__(self, request, client_address, server)
+
+    def log_message(self, format, *args):
+        #Quiet the logs
+        return
+
+    def do_POST(self):
+        responseHandler(self, 405)
+
+    def do_OPTIONS(self):
+
+        tmpHeaders = []
+
+        tmpHeader = {}
+        tmpHeader['key'] = 'Access-Control-Allow-Methods'
+        tmpHeader['value'] = "*"
+        tmpHeaders.append(tmpHeader)
+
+        tmpHeader = {}
+        tmpHeader['key'] = 'Access-Control-Allow-Headers'
+        tmpHeader['value'] = "*"
+        tmpHeaders.append(tmpHeader)
+
+        responseHandler(self, 200, headers=tmpHeaders)
+
+    def do_GET(self):
+
+        try:
+
+            #Ensure the correct key was sent
+            authenticate(self)
+
+            #Get the operation requested by the user
+            operation = parse.urlsplit(self.path).path.replace("/", "")
+
+            if operation == "registration":
+                getRegistration(self)
+                return
+
+            if operation == "operator":
+                getOperator(self)
+                return
+
+            #All other requests get 404
+            responseHandler(self, 404)
+
+        except HTTPErrorResponse as ex:
+            responseHandler(self, ex.status, body={"error": ex.message})
+
+        except HTTPUnauthorizedResponse as ex:
+            responseHandler(self, ex.status)
+
+        except Exception as ex:
+            logger.error({"exception": ex})
+            responseHandler(self, 500, body={"error": "Unknown Error"})
+
+
+class HTTPErrorResponse(Exception):
+
+    #Custom error message wrapper
+
+    def __init__(self, status=500, message="Unknown Error"):
+        self.status = status
+        self.message = message
+        super().__init__(self.status, self.message)
+
+
+class HTTPUnauthorizedResponse(Exception):
+
+    #Custom unauthorized message wrapper
+
+    def __init__(self, status=401):
+        self.status = status
+        super().__init__(self.status)
+
+
+def authenticate(requestHandler):
+
+    #Ensure the header exists
+    if "x-api-key" not in requestHandler.headers:
+        raise HTTPUnauthorizedResponse(status=401)
+
+    #Ensure the header matches
+    if requestHandler.headers['x-api-key'] != settings['api']['x-api-key']:
+        raise HTTPUnauthorizedResponse(status=401)
+
+
+def getRegistration(requestHandler):
+
+    #Parse the query string
+    queryString = parse.parse_qs(parse.urlsplit(requestHandler.path).query)
+
+    #Ensure we have exactly 1 registration or icao_hex in the query string but not both
+    if "registration" not in queryString and "icao_hex" not in queryString:
+        raise HTTPErrorResponse(status=400, message="Either 'icao_hex' or 'registration' parameter is required")
+
+    if "registration" in queryString and "icao_hex" in queryString:
+        raise HTTPErrorResponse(status=400, message="Only one of 'icao_hex' or 'registration' parameter is allowed")
+
+    #Ensure we only have 1 instance of either the registration or icao_hex
+    if "registration" in queryString:
+        if len(queryString['registration']) != 1:
+            raise HTTPErrorResponse(status=400, message="Exactly 1 registration must be supplied")
+        column_name = "registration"
+        query_value = queryString['registration'][0]
+
+    if "icao_hex" in queryString:
+        if len(queryString['icao_hex']) != 1:
+            raise HTTPErrorResponse(status=400, message="Exactly 1 icao_hex must be supplied")
+        column_name = "icao_hex"
+        query_value = queryString['icao_hex'][0]
+
+    #Default to simple data
+    table_name = "simple"
+
+    if "detailed" in queryString:
+        if str(queryString['detailed'][0]).lower() == "true":
+            #Change to detailed data
+            table_name = "registrations"
+
+    registrationDb = mysql.connector.connect(
+        host=settings['mySQL']['uri'],
+        user=settings['mySQL']['username'],
+        password=settings['mySQL']['password'],
+        database=settings['mySQL']['database'])
+
+    mysqlCur = registrationDb.cursor(dictionary=True)
+
+    mysqlCur.execute("SELECT " + table_name + ".data, sources.agency FROM " + table_name + " "\
+                        "INNER JOIN sources ON " + table_name + ".source = sources.unique_id " \
+                        "WHERE " + column_name + " = '" + query_value + "' AND " + table_name + ".deleted is null;")
+
+    result = mysqlCur.fetchall()
+
+    mysqlCur.close()
+    registrationDb.close()
+
+    #Ensure we have have exactly 1 row
+    if len(result) == 1:
+
+        #Move the agency into the result
+        returnValue = json.loads(result[0]['data'])
+        returnValue['source'] = result[0]['agency']
+        
+        responseHandler(requestHandler, 200, body=returnValue)
+        return
+
+    if len(result) == 0:
+        responseHandler(requestHandler, 404)
+        return
+
+    #Default to an error
+    logger.warning("Retrieved " + str(len(result)) + " records from MySQL when querying for registration for '" + "" + "'.  Expected 0 or 1.")
+    raise HTTPErrorResponse(status=409, message="Unexpected database response") 
+
+
+def getOperator(requestHandler):
+
+    #Parse the query string
+    queryString = parse.parse_qs(parse.urlsplit(requestHandler.path).query)
+
+    #Ensure we have an airline_designator in the query string and there is exactly 1 request
+    if "airline_designator" not in queryString:
+        raise HTTPErrorResponse(status=400, message="Parameter 'airline_designator' is required")
+
+    if "airline_designator" in queryString:
+        if len(queryString['airline_designator']) != 1:
+            raise HTTPErrorResponse(status=400, message="Exactly 1 airline_designator must be supplied")
+
+    operatorsDb = mysql.connector.connect(
+        host=settings['mySQL']['uri'],
+        user=settings['mySQL']['username'],
+        password=settings['mySQL']['password'],
+        database=settings['mySQL']['database'])
+
+    mysqlCur = operatorsDb.cursor(dictionary=True)
+
+    mysqlCur.execute("SELECT airline_designator, name, callsign, country FROM operators WHERE airline_designator = '" + str(queryString['airline_designator'][0]) + "' AND deleted is null;")
+
+    result = mysqlCur.fetchall()
+
+    mysqlCur.close()
+    operatorsDb.close()
+
+    #Ensure we have have exactly 1 row
+    if len(result) == 1:
+        responseHandler(requestHandler, 200, body=result[0])
+        return
+
+    if len(result) == 0:
+        responseHandler(requestHandler, 404)
+        return
+
+    #Default to an error
+    logger.warning("Retrieved " + str(len(result)) + " records from MySQL when querying for operator '" + str(queryString['airline_designator'][0]) + "'.  Expected 0 or 1.")
+    raise HTTPErrorResponse(status=409, message="Unexpected database response") 
+       
+
+def responseHandler(requestHandler, status, headers=[], body="", contentType="application/json"):
+
+    #Send the HTTP status code requested
+    requestHandler.send_response(status)
+
+    if len(body) > 0:
+        tmpHeader = {}
+        tmpHeader['key'] = "Content-Type"
+        tmpHeader['value'] = contentType
+        headers.append(tmpHeader)
+
+    tmpHeader = {}
+    tmpHeader['key'] = 'Access-Control-Allow-Origin'
+    tmpHeader['value'] = "*"
+    headers.append(tmpHeader)
+
+    #Send each header to the caller
+    for header in headers:
+        requestHandler.send_header(header['key'], header['value'])
+
+    #Send a blank line to the caller
+    requestHandler.end_headers()
+
+    #Empty the headers
+    headers.clear()
+
+    #Write the response body to the caller
+    if len(body) > 0:
+
+        if contentType == "application/json":
+            requestHandler.wfile.write(json.dumps(body).encode("utf8"))
+        else:
+            requestHandler.wfile.write(body)
+
+
+def setLogLevel(logLevel):
+
+    logLevel = logLevel.lower()
+
+    if logLevel == "debug":
+        logger.setLevel(logging.DEBUG)
+        logger.debug("Logging set to DEBUG.")
+        return
+
+    if logLevel == "error":
+        logger.setLevel(logging.ERROR)
+        logger.error("Logging set to ERROR.")
+        return
+
+    if logLevel == "warning":
+        logger.setLevel(logging.WARNING)
+        logger.warning("Logging set to WARNING.")
+        return
+
+    if logLevel == "critical":
+        logger.setLevel(logging.CRITICAL)
+        logger.critical("Logging set to CRITICAL.")
+        return
+
+
+def setup():
+    global applicationName
+    global settings
+    global logger
+
+    #Define some constants
+    applicationName = "Aircraft Registration and Operator Information API"
+    settings = {}
+
+    try:
+
+        filePath = os.path.dirname(os.path.realpath(__file__))
+
+        #Setup the logger, 10MB maximum log size
+        logger = logging.getLogger(applicationName)
+        formatter = logging.Formatter('%(asctime)s [%(levelname)s] - %(message)s')
+        logHandler = handlers.RotatingFileHandler(os.path.join(filePath, 'api.log'), maxBytes=10485760, backupCount=1)
+        logHandler.setFormatter(formatter)
+        logger.addHandler(logHandler)
+        logger.setLevel(logging.INFO)
+
+        logger.info(applicationName + " started.")
+
+        #Make sure the settings file exists
+        if os.path.exists(os.path.join(filePath, 'settings.json')) == False:
+            raise Exception("Settings file does not exist.  Expected file " + os.path.join(filePath, 'settings.json'))
+
+        #Settings file exists, read it in and verify its contents
+        with open(os.path.join(filePath, 'settings.json')) as settingsFile:
+            settings = json.load(settingsFile)
+
+        if "log_level" in settings:
+            setLogLevel(settings['log_level'])
+
+        if "api" not in settings:
+            raise Exception ("api object is missing from settings.json")
+
+        if "x-api-key" not in settings['api']:
+            raise Exception ("Missing api -> x-api-key in settings.json")
+
+        if settings['api']['x-api-key'] == "":
+            raise Exception ("Empty api -> x-api-key in settings.json")
+
+        if settings['api']['x-api-key'] == "5d95bb51-64b1-4269-b812-2e20e59cb3c5":
+            raise Exception ("Default x-api-key used.  Please change the value for x-api-key")
+
+        if "port" not in settings['api']:
+            raise Exception ("Missing api -> port in settings.json")
+
+        if str(settings['api']['port']).isnumeric() != True:
+            raise Exception ("Invalid api -> port in settings.json")
+
+        if "mySQL" not in settings:
+            raise Exception ("mySQL object is missing from settings.json")
+
+        if "uri" not in settings['mySQL']:
+            raise Exception ("Missing mySQL -> uri in settings.json")
+
+        if "database" not in settings['mySQL']:
+            raise Exception ("Missing mySQL -> database in settings.json")
+
+        if "username" not in settings['mySQL']:
+            raise Exception ("Missing mySQL -> username in settings.json")
+
+        if "password" not in settings['mySQL']:
+            raise Exception ("Missing mySQL -> password in settings.json")
+        
+
+    except Exception as ex:
+        logger.error(ex)
+        exitApp(1)
+
+
+def exitApp(exitCode=None):
+
+    #Force the log level to info
+    logger.setLevel(logging.INFO)
+
+    if exitCode is None:
+        exitCode = 0
+
+    if exitCode == 0:
+        logger.info(applicationName + " finished successfully.")
+
+    if exitCode != 0:
+        logger.info("Error; Exiting with code " + str(exitCode))
+
+    sys.exit(exitCode)
+
+
+def main():
+
+    #Start the HTTP server
+    try:
+
+        logger.info("Starting HTTP server on port " + str(settings['api']['port']))
+
+        #Create the webserver
+        httpd = socketserver.TCPServer(("", settings['api']['port']), RequestHandler)
+
+        #Serve clients until stopped
+        httpd.serve_forever()
+
+    except sigKill:
+        #Kill the http server and clean up open connections
+        httpd.server_close()
+        exitApp(0)       
+
+    except KeyboardInterrupt:
+        #Kill the http server and clean up open connections
+        httpd.server_close()
+        exitApp(0)
+
+    except Exception as ex:
+        logger.error(ex)
+
+        
+if __name__ == "__main__":
+
+    signal.signal(signal.SIGTERM, handle_interrupt)
+    setup()
+    main()
---
+++ readme.md
@@ -0,0 +1,221 @@
+# Aircraft Registration and Operator Information
+Enrich ADS-B information with aircraft registration and operator data
+
+## What Does it Do?
+Aircraft Registration and Operator Information (AROI) imports aircraft registrations and operator information from various sources, stores them in MySQL, and makes that data available to retrieve via API microservice.  It is designed to work as microservice for [SkyFollower](https://github.com/BrentIO/SkyFollower).
+
+## How does it Work?
+
+
+
+## Prerequisites
+AROI can run on pretty much any operating system that runs Python3, but this document will be focused exclusively on explaining how to do so under Ubuntu 20.04 (Focal Fossa).
+
+
+### Required packages
+Using apt, install the prerequisite packages, if you don't already have them installed:
+```
+sudo apt-get install -y python3 python3-pip mysql-server
+```
+`TO DO: Make sure this list is correct`
+
+Python also requires a number of packages that must be installed:
+```
+sudo pip3 install requests progress yaspin mysql-connector-python pymongo
+```
+
+### MySQL Configuration
+Log into MySQL:
+
+```
+sudo mysql
+```
+
+
+Change the root user to use a native password, in this case the root password will be changed to `password`, but you should choose something more secure:
+```
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+```
+
+If you are running MySQL on a separate server, you will need to allow non-localhost connections in MySQL.
+
+>   _Only perform this step if you are running MySQL on a separate server, or if you want to use MySQL Workbench on a separate computer._ 
+
+```
+sudo sed -i 's/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+```
+
+Restart MySQL:
+```
+sudo systemctl restart mysql
+```
+
+Login to MySQL with the updated root account, and enter the password when prompted:
+```
+mysql -u root -p
+```
+
+Create the database, which will be named `AROI`:
+```
+mysql> CREATE DATABASE AROI;
+```
+> If you use a different database name, adjust the steps below in this guide as well as the settings.json file.
+
+Create the new account, in this case the user will be named `aroi` and the password will be `my_clear_text_password`:
+```
+CREATE USER 'aroi'@'%' IDENTIFIED BY 'my_clear_text_password';
+```
+
+
+Grant the user above to have super admin privileges on the database we created, `AROI`:
+```
+GRANT ALL PRIVILEGES ON AROI.* TO 'aroi'@'%' WITH GRANT OPTION;
+```
+
+
+Flush the privileges table to pick up the changes:
+
+```
+FLUSH PRIVILEGES;
+```
+
+Create the tables in the database using the username, password, and database created in the previous steps:
+```
+mysql -u aroi -p AROI < /etc/P5Software/AROI/mysql_create.sql
+```
+
+
+## Download and Install AROI
+Clone AROI from GitHub:
+```
+sudo git clone https://github.com/BrentIO/Aircraft-Registration-and-Operator-Information.git /etc/P5Software/AROI 
+```
+
+## Required Configuration
+You *must* configure these settings in the settings.json file.
+
+```
+sudo nano /etc/P5Software/AROI/settings.json
+```
+
+| Parameter     | Default   | Description |
+----------------|-----------|-------------|
+|`mySQL -> password`| my_clear_text_password | Clear text password to use when connecting to MySQL |
+| `api -> x-api-key` | 5d95bb51-64b1-4269-b812-2e20e59cb3c5 | Key used when to provide minimal security when accessing the API.   Any string of characters maybe used, or a new UUID generated. |
+
+
+## Optional Configuration
+The settings.json file contains all of the user-configurable settings for AROI.  In addition to those in the Required Configuration section, optional parameters are described below.
+
+| Parameter | Default | Description |
+|-----------|---------|-------------|
+|`mySQL -> uri`| localhost | MySQL Server IP address or domain name.  Special configuration is required in MySQL to allow remote connections (disabled in MySQL by default) if you are not running AROI and MySQL on the same device.|
+|`mySQL -> database`| AROI | The MySQL database name|
+|`mySQL -> username`| aroi | Username to use when connecting to MySQL|
+|`api -> port`| 8480 | Port number for the API server, as an integer.|
+|`skip_download`| false | Indicates if the download should be skipped when importing a new file.  If omitted, defaults to `false`.  For debugging purposes only.|
+| `local_database_mode` | memory | Determines if the cached database is stored in memory or disk.  Options are `disk` or `memory`.  If using disk, be mindful that this will cause significant writes, may cause dramatic reduction in speed, and is intended for debugging purposes only.  The local database is only used when actively importing data from an external source.  If omitted, defaults to `memory`.|
+|`limit`| false | Limits the number of records that will be imported to only 500 records.  If omitted, defaults to `false`.  For debugging purposes only.|
+
+
+## Service Installation
+
+Copy the service file to the systemctl directory
+```
+sudo mv /etc/P5Software/AROI/AROI.service /lib/systemd/system/
+```
+
+Reload the systemctrl daemon to pick up the new AROI service
+```
+sudo systemctl daemon-reload
+```
+
+Enable the service to run
+```
+sudo systemctl enable AROI.service
+```
+
+Start the service
+```
+sudo systemctl start AROI.service
+```
+
+## SQL Table Creation
+`TO DO: PUT STUFF HERE`
+
+## Import Agency Data
+To load the data into MySQL, you need to download data from at least one agency.  Detailed data comes from the FAA and Transport Canada.  Summary data and operator data comes from Mictronics, and is not an official source.
+
+Data is retained forever and it is assumed the latest file imported contains the newest data.
+
+| Agency | Data Type | Import Script | Release Frequency | URL |
+|--------|-----------|---------------|-------------------|-----|
+| US FAA | Detailed | us-faa.py | Daily at 23:30 Central | https://registry.faa.gov/database/ReleasableAircraft.zip |
+| Transport Canada | Detailed | ca-tc.py | Unknown | https://wwwapps.tc.gc.ca/Saf-Sec-Sur/2/CCARCS-RIACC/download/ccarcsdb.zip |
+Mictronics IndexedDB | Summary | mictronics-indexeddb.py | Weekly on Sundays | https://www.mictronics.de/aircraft-database/indexedDB.php |
+
+
+### Mictronics IndexedDB
+
+_Use of this agency is strongly recommended_
+
+Mictronics collects and releases a data extract based on a number of sources, which are not known.  Because they collect data worldwide, it contains most active aircraft on the globe with a high degree of quality.
+
+This appears to be a volunteer effort, so be cognizant that this data is important and don't spam their servers with unnecessary requests.
+
+`sudo crontab -e`
+
+Download the dataset and update the data weekly on Tuesdays at 05:10:
+```
+10 5 * * 2 python3 /etc/P5Software/AROI/mictronics-indexeddb.py https://www.mictronics.de/aircraft-database/indexedDB.php
+```
+
+Force the script to run now:
+
+`sudo python3 /etc/P5Software/AROI/mictronics-indexeddb.py https://www.mictronics.de/aircraft-database/indexedDB.php`
+
+
+### United States FAA
+
+_Use of this agency is optional_
+
+Databases as old as 2017 can be downloaded from the FAA website and processed by specifying their URL when calling the python script.  _If you choose to do this, be sure to do this before running the current file._
+
+The FAA releases databases daily, but there are usually a small number of updates.  It is also a very large dataset that will take a non-trivial amount of time to import, so weekly updates should be sufficient for most users.
+
+`sudo crontab -e`
+
+Download the dataset and update the data weekly on Saturdays at 05:10:
+```
+10 5 * * 6 python3 /etc/P5Software/AROI/us-faa.py https://registry.faa.gov/database/ReleasableAircraft.zip
+```
+
+Force the script to run now:
+
+`sudo python3 /etc/P5Software/AROI/us-faa.py https://registry.faa.gov/database/ReleasableAircraft.zip`
+
+### Transport Canada
+
+_Use of this agency is optional_
+
+`sudo crontab -e`
+
+Download the dataset and update the data weekly on Sundays at 05:10:
+```
+10 5 * * 7 python3 /etc/P5Software/AROI/ca-tc.py https://wwwapps.tc.gc.ca/Saf-Sec-Sur/2/CCARCS-RIACC/download/ccarcsdb.zip
+```
+
+Force the script to run now:
+
+`sudo python3 /etc/P5Software/AROI/ca-tc.py https://wwwapps.tc.gc.ca/Saf-Sec-Sur/2/CCARCS-RIACC/download/ccarcsdb.zip`
+
+
+
+
+## FAQ
+- Can I host this on a public website?
+  - You can, but it's not a good idea -- the HTTP server is not designed to handle significant volume and implements only minimal security.
+  - Some agencies license the use of their data, and doing so could be a violation of that license.
+
+## Credits and Thanks
+- [Mictronics](https://github.com/mictronics) for the awesome work with the IndexedDB database.  They also make a really great ADS-B decoder.
